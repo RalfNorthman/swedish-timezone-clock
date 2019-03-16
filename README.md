b1bd9d53b9f1bd81387eb3fb552975791bbd8661
Ralf Northman <ralf.northman@bredband.net>
2019-03-16 14:29:15 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+## Simple clock with timezone support in elm.
+It uses swedish formatting.
+
+To change to english just swap 'swedish' to 'english' in the DateFormat.Language import and the myFormat function.
+
+Created with create-elm-app. See separate .md for details.
---
+++ src/Main.elm
@@ -0,0 +1,129 @@
+module Main exposing (..)
+
+import Browser
+import Time exposing (Posix, Zone)
+import Html exposing (Html)
+import Element exposing (..)
+import TimeZone exposing (getZone)
+import Task exposing (attempt)
+import DateFormat as Format
+import DateFormat.Language exposing (swedish)
+
+
+---- MODEL ----
+
+
+type alias Model =
+    { zoneName : String
+    , zone : Zone
+    , time : Posix
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { zoneName = "Nothing yet."
+      , zone = Time.utc
+      , time = Time.millisToPosix 0
+      }
+    , attempt GetZone getZone
+    )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = GetZone (Result TimeZone.Error ( String, Zone ))
+    | Tick Posix
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GetZone (Ok ( name, zone )) ->
+            ( { model | zoneName = name, zone = zone }, Cmd.none )
+
+        GetZone (Err error) ->
+            ( { model | zoneName = "Attempt failed." }, Cmd.none )
+
+        Tick posix ->
+            ( { model | time = posix }, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+myFormat : List Format.Token -> Model -> String
+myFormat tokens model =
+    Format.formatWithLanguage
+        swedish
+        tokens
+        model.zone
+        model.time
+
+
+clockFormat : Model -> String
+clockFormat model =
+    myFormat
+        [ Format.hourMilitaryFixed
+        , Format.text ":"
+        , Format.minuteFixed
+        , Format.text ":"
+        , Format.secondFixed
+        ]
+        model
+
+
+dateFormat : Model -> String
+dateFormat model =
+    myFormat
+        [ Format.dayOfMonthSuffix
+        , Format.text " "
+        , Format.monthNameFull
+        ]
+        model
+
+
+myText : String -> Element msg
+myText string =
+    el [ centerX ] <| text string
+
+
+view : Model -> Html Msg
+view model =
+    layout [] <|
+        column
+            [ padding 20
+            , spacing 8
+            ]
+            [ myText model.zoneName
+            , myText <| clockFormat model
+            , myText <| myFormat [ Format.dayOfWeekNameFull ] model
+            , myText <| dateFormat model
+            ]
+
+
+
+---- SUBSCRIPIONS ----
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every 1000 Tick
+
+
+
+---- PROGRAM ----
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { view = view
+        , init = \_ -> init
+        , update = update
+        , subscriptions = subscriptions
+        }
